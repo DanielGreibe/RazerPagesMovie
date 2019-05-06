e89e912f4c761d305b66ee21a7288e30f6a8509b
Daniel Greibe <danielgreibe@gmail.com>
2019-05-06 07:55:42 +0200

    Initial Commit

---
+++ RazerPagesMovie/Data/RazerPagesMovieContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace RazerPagesMovie.Models
+{
+    public class RazerPagesMovieContext : DbContext
+    {
+        public RazerPagesMovieContext (DbContextOptions<RazerPagesMovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<RazerPagesMovie.Models.Movie> Movie { get; set; }
+    }
+}
